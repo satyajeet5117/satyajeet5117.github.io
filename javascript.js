1d1591c2df19c6bf173c204552050db99179d3f1
satyajeet5117 <satyajeet.xworkz@gmail.com>
2022-06-21 10:58:19 +0530

    4 method calling

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+
+console.log("java script")
+            var a=10
+            console.log(a)
+            console.log(typeof(a))
+            a="java script"
+            console.log(a)
+            console.log(typeof(a))
+            var b;
+            console.log(b)
+            console.log(b)
+            console.log(typeof(b))
+            var a;
+            console.log(a)
+
+            function disp(){
+                console.log("calling display function")
+            }
+            function call(){
+                console.log("calling call function")
+            }    
+            function apply(){
+                console.log("calling apply function")
+            }    
+            function bind(){
+                console.log("calling bind function")
+            }    
